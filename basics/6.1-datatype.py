5b3129069a7e1b4f47225d963d73666bb47752b0
Ninad Bhoir <ninad>
2026-03-04 23:12:42 +0530

    data types

---
+++ basics/6.1-datatype.py
@@ -0,0 +1,23 @@
+#data types
+# String:-
+#     - sequence of chracterstics
+# Literal:-
+#     - single quote
+name ='ninad'
+#     - double quote
+city ="Mumbai"
+#     - triple quotation
+address ="""
+    Kamakshi Dham,
+    Sec 19C, Koperkhairane, Navi Mumbai 
+    - 400709
+"""
+print(name)
+print(city)
+print(address)
+print(*"*2")
+print("Hello Dear, \nwe are \tplease to offer this")
+print(r"C:\users\abcd\desktop\newfolder")
+
+msg = f"Dear {name}\n Your city is {city}"
+print(msg)
