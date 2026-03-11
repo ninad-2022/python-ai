5d8c692d88d4b4ee770f793edc2cdb240ab42e3c
Ninad Bhoir <ninad>
2026-03-11 22:12:43 +0530

    learning: compplex datatype

---
+++ basics/12.2-complex.py
@@ -0,0 +1,15 @@
+a = 20 + 20j
+b = complex(10)
+c = complex(10.5)
+print(a)
+print(b)
+print(c)
+print(type(a))
+
+# extract part of complex number 
+print(a.real)
+print(a.imag)
+
+#coordinates = [lat, long]
+
+print((2+3j)+ (5+2j))
