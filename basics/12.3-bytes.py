f4e5a5a9de5445b3c3310393d8ffee96dfaf2d3b
Ninad Bhoir <ninad>
2026-03-12 22:16:10 +0530

    byes

--- basics/12.3-bytes.py
+++ basics/12.3-bytes.py
@@ -17,3 +17,23 @@ print("c:",c)
 x =b'\xe0\xa4\xe0\xb0\xa4\xe0\xa5'
 print("x:",x)
 print(type(x))
+
+# bytes to string 
+ss = c.decode("utf-8")
+print("ss",ss)
+print(type(ss))
+
+# bytes is immutable
+d = "hello" 
+print("d[0]:",d[0])
+
+print("-"*10, "bytesarray()", "-"*10)
+name = bytearray([65,66,77])
+# name[0]=90
+print("name", name)
+name.append(84) #add an element at the end
+print("name:append", name)
+name.extend([84,90,65]) #add en element at the end
+print("name:extend", name)
+name.reverse()
+print("name: reverse", name)
