bd2da59f8a6a8a585dc4c9dccf63134badde0f21
Ninad Bhoir <ninad>
2026-03-20 21:16:46 +0530

    develop: set

---
+++ basics/16-set.py
@@ -0,0 +1,10 @@
+a = {}
+b = {10,11,23,13}
+c = {10,11,23,13,13}
+print(type(a))
+print(type(b))
+print(type(c))
+
+# set and dic sytax are same. 
+#     - when we use quoma seprated then it becomes set
+#     - when we use key value pair then it becomes dic
