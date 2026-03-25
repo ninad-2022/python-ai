918e3ab10e8b196d3c872145ed846986a7ba96c6
Ninad Bhoir <ninad>
2026-03-25 21:00:35 +0530

    develop: set

--- basics/16-set.py
+++ basics/16-set.py
@@ -7,4 +7,11 @@ print(type(c))
 
 # set and dic sytax are same. 
 #     - when we use quoma seprated then it becomes set
+d = {10, True, "Hii", (10,20)}
+print(d)
+print(type(d))
+
 #     - when we use key value pair then it becomes dic
+e = { "name":"ninad", "city":"mumbai", "address":{ "pincode":234234 }}
+print(e)
+print(type(e))
