cadb425dfd570045cda649066c9246e30a246716
Ninad Bhoir <ninad>
2026-03-25 22:25:42 +0530

    develop: comprehension in variable

--- basics/17-comprehension.py
+++ basics/17-comprehension.py
@@ -26,3 +26,22 @@ print(oddEven)
 y = 1
 if y == 0:
     print(y)
+
+# comprehension for dictory
+# value will came from this "i*i for i in range(20) if i >0" and get assign into the i 
+xx = {i: i*i for i in range(20) if i >0}
+print(xx)
+
+# create dic of city with no of character in the name
+
+cities =["mumbai", "pune", "nagpur", "raigad"]
+count = { city: len(city) for city in cities}
+print(cities)
+print(count)
+
+# create dictory from two list 
+places = ["vashi", "ghansoli", "panvel"]
+pincode = [1910, 2434, 35445, 43534]
+# zip is used to combine tuple list 
+locations = { k:v for k, v in zip(places, pincode)}
+print(locations)
